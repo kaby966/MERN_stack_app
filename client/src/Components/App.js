096ae2e49ba34e72bc670ab3d1beb9d4d5499537
unknown <kapil3rde@gmail.com>
2021-01-29 14:58:13 +0530

    MERN stack app project 29/01/2021 3.00PM

---
+++ client/src/Components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Common/Header';
+import Login from '../Components/Auth/Login';
+import Signup from '../Components/Auth/Signup';
+import NotFound from '../Components/Pages/NotFound';
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/signup" component={Signup}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </main>
+    </Router>
+  )
+
+}
+
+export default App;
---
+++ client/src/Components/Common/Header.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Logo</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/login">SignIn</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/signup">SignUp</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
